9c3ddb963cbf860f365bb85fce068cdf49299f82
MistiXe <hohomahdjoub@gmail.com>
2021-10-20 14:36:16 +0200

    New UI

--- application/src/App.js
+++ application/src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import ClimbingBoxLoader from 'react-spinners/ClimbingBoxLoader';
 
-function App() {
+var zz = 6
+
+
+function App(){
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+
+    }, 5000)
+  }, [] )
+  var xx =5
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        { loading ? (
+
+            <><ClimbingBoxLoader size="30" color={"#36D7B7"} loading={loading} /><h1>Chargement </h1></>
+
+        ) : (
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1>E-COMM Launcher </h1>
+              <button  className = "btn">Créer une partie </button>
+              <button className = "btn">Rejoindre une partie</button>
+            </header>
+        )
+        }
+      </div>
   );
 }
 
