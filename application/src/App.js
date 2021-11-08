b714701246331cdd3769b2fa5f4ab20b5b96b30b
fgerber <florent.gerberpro@gmail.com>
2021-11-08 17:54:45 +0100

    update

--- application/src/App.js
+++ application/src/App.js
@@ -48,7 +48,7 @@ function App(){
         )
     }
     else {
-        page = <Main />
+        page = <Main returnMenuP = {() => setStart(false)} />
     }
     return (
         <div className="App">
--- application/src/threadGame.js
+++ application/src/threadGame.js
@@ -1,25 +1,11 @@
 import React, {useState} from 'react';
 import './App.css';
-import App from "./App";
-import BtnContext from "./PageDefault/BtnContext";
-
-
-
-let etapes =[
-    <p>Upon receiving a Purchase Order from 123 Importing, the exporter (Zen Export International) sends a
-        Pro Forma Invoice (also referred to as a Commercial Invoice)</p>,
-    <p>123 Importing applies for the letter of credit with its bank in New York.</p>,
-    <p>The bank accepts this application and proceeds for the letter of credit, and sends the irrevocable letter
-        of credit to the bank in Japan, requesting a confirmation.</p>]
+import BtnContext from "./Components/BtnContext";
 
 
 function Main(props){
     const[etape, setEtape] = useState(-5);
     let centre;
-
-
-
-
 
     if(etape===0) {
 
@@ -34,7 +20,7 @@ function Main(props){
                 <nav className="navbar">
                     <ul className="navbar-nav">
                     <li className="navbar-item" >
-                        <btn className="nav-link">
+                        <btn className="nav-link" onClick = {props.returnMenuP}>
                             <svg
                                 aria-hidden="true"
                                 focusable="false"
@@ -47,15 +33,15 @@ function Main(props){
                             >
                                 <g className="fa-group">
                                     <path
-                                        fill="currentColor"
-                                        d="M224 273L88.37 409a23.78 23.78 0 0 1-33.8 0L32 386.36a23.94 23.94 0 0 1 0-33.89l96.13-96.37L32 159.73a23.94 23.94 0 0 1 0-33.89l22.44-22.79a23.78 23.78 0 0 1 33.8 0L223.88 239a23.94 23.94 0 0 1 .1 34z"
-                                        className="fa-secondary"
-                                    ></path>
+                                    fill="currentColor"
+                                    d="M224 273L88.37 409a23.78 23.78 0 0 1-33.8 0L32 386.36a23.94 23.94 0 0 1 0-33.89l96.13-96.37L32 159.73a23.94 23.94 0 0 1 0-33.89l22.44-22.79a23.78 23.78 0 0 1 33.8 0L223.88 239a23.94 23.94 0 0 1 .1 34z"
+                                    className="fa-secondary"
+                                    />
                                     <path
-                                        fill="currentColor"
-                                        d="M415.89 273L280.34 409a23.77 23.77 0 0 1-33.79 0L224 386.26a23.94 23.94 0 0 1 0-33.89L320.11 256l-96-96.47a23.94 23.94 0 0 1 0-33.89l22.52-22.59a23.77 23.77 0 0 1 33.79 0L416 239a24 24 0 0 1-.11 34z"
-                                        className="fa-primary"
-                                    ></path>
+                                    fill="currentColor"
+                                    d="M415.89 273L280.34 409a23.77 23.77 0 0 1-33.79 0L224 386.26a23.94 23.94 0 0 1 0-33.89L320.11 256l-96-96.47a23.94 23.94 0 0 1 0-33.89l22.52-22.59a23.77 23.77 0 0 1 33.79 0L416 239a24 24 0 0 1-.11 34z"
+                                    className="fa-primary"
+                                    />
                                 </g>
                             </svg>
                             <span className="link-text">Menu</span>
@@ -78,15 +64,15 @@ function Main(props){
                                 >
                                     <g className="fa-group">
                                         <path
-                                            fill="currentColor"
-                                            d="M448 96h-64l-64-64v134.4a96 96 0 0 0 192 0V32zm-72 80a16 16 0 1 1 16-16 16 16 0 0 1-16 16zm80 0a16 16 0 1 1 16-16 16 16 0 0 1-16 16zm-165.41 16a204.07 204.07 0 0 0-34.59 2.89V272l-43.15-64.73a183.93 183.93 0 0 0-44.37 26.17L192 304l-60.94-30.47L128 272v-80a96.1 96.1 0 0 0-96-96 32 32 0 0 0 0 64 32 32 0 0 1 32 32v256a64.06 64.06 0 0 0 64 64h176a16 16 0 0 0 16-16v-16a32 32 0 0 0-32-32h-32l128-96v144a16 16 0 0 0 16 16h32a16 16 0 0 0 16-16V289.86a126.78 126.78 0 0 1-32 4.54c-61.81 0-113.52-44.05-125.41-102.4z"
-                                            className="fa-secondary"
-                                        ></path>
+                                        fill="currentColor"
+                                        d="M448 96h-64l-64-64v134.4a96 96 0 0 0 192 0V32zm-72 80a16 16 0 1 1 16-16 16 16 0 0 1-16 16zm80 0a16 16 0 1 1 16-16 16 16 0 0 1-16 16zm-165.41 16a204.07 204.07 0 0 0-34.59 2.89V272l-43.15-64.73a183.93 183.93 0 0 0-44.37 26.17L192 304l-60.94-30.47L128 272v-80a96.1 96.1 0 0 0-96-96 32 32 0 0 0 0 64 32 32 0 0 1 32 32v256a64.06 64.06 0 0 0 64 64h176a16 16 0 0 0 16-16v-16a32 32 0 0 0-32-32h-32l128-96v144a16 16 0 0 0 16 16h32a16 16 0 0 0 16-16V289.86a126.78 126.78 0 0 1-32 4.54c-61.81 0-113.52-44.05-125.41-102.4z"
+                                        className="fa-secondary"
+                                        />
                                         <path
-                                            fill="currentColor"
-                                            d="M376 144a16 16 0 1 0 16 16 16 16 0 0 0-16-16zm80 0a16 16 0 1 0 16 16 16 16 0 0 0-16-16zM131.06 273.53L192 304l-23.52-70.56a192.06 192.06 0 0 0-37.42 40.09zM256 272v-77.11a198.62 198.62 0 0 0-43.15 12.38z"
-                                            className="fa-primary"
-                                        ></path>
+                                        fill="currentColor"
+                                        d="M376 144a16 16 0 1 0 16 16 16 16 0 0 0-16-16zm80 0a16 16 0 1 0 16 16 16 16 0 0 0-16-16zM131.06 273.53L192 304l-23.52-70.56a192.06 192.06 0 0 0-37.42 40.09zM256 272v-77.11a198.62 198.62 0 0 0-43.15 12.38z"
+                                        className="fa-primary"
+                                        />
                                     </g>
                                 </svg>
                                 <BtnContext />
@@ -108,15 +94,15 @@ function Main(props){
                                 >
                                     <g className="fa-group">
                                         <path
-                                            fill="currentColor"
-                                            d="M560,128H528a15.99954,15.99954,0,0,0-16,16v80H480V176a15.99954,15.99954,0,0,0-16-16H416V96h48a16.00079,16.00079,0,0,0,16-16V48a15.99954,15.99954,0,0,0-16-16H432a15.99954,15.99954,0,0,0-16,16V64H368a15.99954,15.99954,0,0,0-16,16v48H224V80a15.99954,15.99954,0,0,0-16-16H160V48a15.99954,15.99954,0,0,0-16-16H112A15.99954,15.99954,0,0,0,96,48V80a16.00079,16.00079,0,0,0,16,16h48v64H112a15.99954,15.99954,0,0,0-16,16v48H64V144a15.99954,15.99954,0,0,0-16-16H16A15.99954,15.99954,0,0,0,0,144V272a16.00079,16.00079,0,0,0,16,16H64v80a16.00079,16.00079,0,0,0,16,16h48v80a16.00079,16.00079,0,0,0,16,16h96a16.00079,16.00079,0,0,0,16-16V432a15.99954,15.99954,0,0,0-16-16H192V384H384v32H336a15.99954,15.99954,0,0,0-16,16v32a16.00079,16.00079,0,0,0,16,16h96a16.00079,16.00079,0,0,0,16-16V384h48a16.00079,16.00079,0,0,0,16-16V288h48a16.00079,16.00079,0,0,0,16-16V144A15.99954,15.99954,0,0,0,560,128ZM224,320H160V224h64Zm192,0H352V224h64Z"
-                                            className="fa-secondary"
-                                        ></path>
+                                        fill="currentColor"
+                                        d="M560,128H528a15.99954,15.99954,0,0,0-16,16v80H480V176a15.99954,15.99954,0,0,0-16-16H416V96h48a16.00079,16.00079,0,0,0,16-16V48a15.99954,15.99954,0,0,0-16-16H432a15.99954,15.99954,0,0,0-16,16V64H368a15.99954,15.99954,0,0,0-16,16v48H224V80a15.99954,15.99954,0,0,0-16-16H160V48a15.99954,15.99954,0,0,0-16-16H112A15.99954,15.99954,0,0,0,96,48V80a16.00079,16.00079,0,0,0,16,16h48v64H112a15.99954,15.99954,0,0,0-16,16v48H64V144a15.99954,15.99954,0,0,0-16-16H16A15.99954,15.99954,0,0,0,0,144V272a16.00079,16.00079,0,0,0,16,16H64v80a16.00079,16.00079,0,0,0,16,16h48v80a16.00079,16.00079,0,0,0,16,16h96a16.00079,16.00079,0,0,0,16-16V432a15.99954,15.99954,0,0,0-16-16H192V384H384v32H336a15.99954,15.99954,0,0,0-16,16v32a16.00079,16.00079,0,0,0,16,16h96a16.00079,16.00079,0,0,0,16-16V384h48a16.00079,16.00079,0,0,0,16-16V288h48a16.00079,16.00079,0,0,0,16-16V144A15.99954,15.99954,0,0,0,560,128ZM224,320H160V224h64Zm192,0H352V224h64Z"
+                                        className="fa-secondary"
+                                        />
                                         <path
-                                            fill="currentColor"
-                                            d="M160,320h64V224H160Zm192-96v96h64V224Z"
-                                            className="fa-primary"
-                                        ></path>
+                                        fill="currentColor"
+                                        d="M160,320h64V224H160Zm192-96v96h64V224Z"
+                                        className="fa-primary"
+                                        />
                                     </g>
                                 </svg>
                                 <span className="link-text">User</span>
@@ -148,8 +134,8 @@ function Main(props){
 }
 
 function Accueil(props){
-    let txtIntro = "Bienvenue dans notre Jeu";
-    let url = "http://streaming.tdiradio.com:8000/house.mp3";
+    let txtIntro = "Welcome in E-COMM !";
+    let url = "https://streaming.tdiradio.com:8000/house.mp3";
     var audio = new Audio(url);
     //audio.play()
 
@@ -161,7 +147,7 @@ function Accueil(props){
 
             </div>
             <div id ="suite">Are you ready ?</div>
-            <button id="next" onClick={props.demarrer}>Jouer</button>
+            <button id="next" onClick={props.demarrer}>Play</button>
         </>
 
 
@@ -259,8 +245,8 @@ function EtapeJeu(props){
             <>
            <center> <p>Choose the right step.</p>
             <div id ="suite"><DevinerSuite className = "acc" suivante={props.suivante}
-                                           possible={[{id: 1, name: "Salut", text: "ça larche"},{id: 2, name: "Salut1", text: "ça lavdvfdrche"},{id: 3, name: "S2t", text: "ça leggdarche"},{id: 4, name: "Sa3ut", text: "ça largfdgdche"}]}
-                                           reponse={'2'} /> </div></center> </>)
+                                           possible={[{id: 1, text: "je suis la première carte"},{id: 2, text: "je suis la deuxième carte"},{id: 3, text: "je suis la troisième carte"},{id: 4, text: "je suis la quatrième quatre"}]}
+                                           reponse={2} /> </div></center> </>)
     } else if(props.etape === 2){
         jeu = (
             <div id = "divformu"><Formulaire  suivante={props.suivante}
@@ -272,27 +258,27 @@ function EtapeJeu(props){
     } else if(props.etape === 3){
         jeu = (
             <div id = "suite"> <DevinerSuite className = "acc" suivante={props.suivante}
-                                             possible={['1', '2', '3','4']}
-                                             reponse={'1'} /></div>
+                                             possible={[{id: 1, text: "je suis la première carte"},{id: 2, text: "je suis la deuxième carte"},{id: 3, text: "je suis la troisième carte"},{id: 4, text: "je suis la quatrième quatre"}]}
+                                             reponse={1} /></div>
 
         )
     }else if(props.etape === 4){
         jeu = (
             <div id = "suite"> <DevinerSuite className = "acc" suivante={props.suivante}
-                                             possible={['1', '2', '3','4']}
-                                             reponse={'1'} /></div>
+                                             possible={[{id: 1, text: "je suis la première carte"},{id: 2, text: "je suis la deuxième carte"},{id: 3, text: "je suis la troisième carte"},{id: 4, text: "je suis la quatrième quatre"}]}
+                                             reponse={1} /></div>
 
         )}else if(props.etape === 5){
         jeu = (
             <div id = "suite"> <DevinerSuite className = "acc" suivante={props.suivante}
-                                             possible={['1', '2', '3','4']}
-                                             reponse={'4'} /></div>
+                                             possible={[{id: 1, text: "je suis la première carte"},{id: 2, text: "je suis la deuxième carte"},{id: 3, text: "je suis la troisième carte"},{id: 4, text: "je suis la quatrième quatre"}]}
+                                             reponse={4} /></div>
 
         )}else if(props.etape === 6){
             jeu = (
                 <div id = "suite"> <DevinerSuite className = "acc" suivante={props.suivante}
-                                                 possible={['1', '2', '3','4']}
-                                                 reponse={'3'} /></div>
+                                                 possible={[{id: 1, text: "je suis la première carte"},{id: 2, text: "je suis la deuxième carte"},{id: 3, text: "je suis la troisième carte"},{id: 4, text: "je suis la quatrième quatre"}]}
+                                                 reponse={3} /></div>
 
             )}
     else if( props.etape === 17){
@@ -346,11 +332,11 @@ function DevinerSuite(props){
 
             <>
 
-            <div className= "divcont"onClick={() => check(txt.name)}>
+            <div className= "divcont" onClick={() => check(txt.id)}>
                 <div className="cards">
                     <div className="cont">
                         <h2>{txt.id}</h2>
-                        <h3>Card {txt.name}</h3>
+                        <h3>Card {txt.id}</h3>
                         <p>{txt.text}</p>
 
 
