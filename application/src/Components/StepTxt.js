72618b7cd13a4f39f02c0170fd2fab547e531ac8
MistiXe <hohomahdjoub@gmail.com>
2021-11-09 11:49:18 +0100

    Patches E-COMM V_0.3
    Time Bar.
    UI settings updated.

---
+++ application/src/Components/StepTxt.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+
+const StepTxt = ["1. Upon receiving a Purchase Order from 123 Importing, the exporter (Zen Export International) sends a\n" +
+"Pro Forma Invoice (also referred to as a Commercial Invoice)",
+
+"2. 123 Importing applies for the letter of credit with its bank in New York",
+
+"3. The bank accepts this application and proceeds for the letter of credit, and sends the irrevocable letter of credit to the bank in Japan, requesting a confirmation.",
+
+"4. The bank in Japan presents the negotiable letter of credit to the beneficiary (exporter), Zen Export International.",
+
+
+    "5. The exporter (Zen Export) reviews all the conditions listed on the letter of credit.",
+
+
+
+
+"6. Zen Export contacts its freight forwarder to confirm that the goods can be shipped prior to the expiration date. If Zen Export is not able to comply with one or more of the conditions, it will alert the importer (123) immediately, with a request to have the letter of credit amended.",
+
+ "7. The exporter (Zen Export) arranges with the freight forwarder to deliver the goods to the appropriate port of shipment.",
+
+
+
+    "8. When transferring the cargo to the international freight forwarder, the exporter will usually prepare the shipper's letter of instructions instructing the freight forwarder on how and where to send the export shipment (transportation and documentation instructions).",
+
+
+"9. After the goods are loaded and placed onboard the vessel for shipment to New York, the freight forwarder completes the necessary documentation and receives the necessary documentation from the shipping carrier.",
+
+
+"10. The key documents returned to the forwarder is the original set of onboard ocean bill of lading, consigned to the order of the bank, and marked Notify Applicant-123 Importing Company, New York",
+
+
+"11. The B/L serves as a receipt issued by the shipping company that a shipment has been received and loaded onboard the vesse for shipment to the port of destination (New York). The B/L will also be required by the importer (123 Importing) in order to clear the goods through Customs and receive them from the shipping company.",
+
+"12. The exporter for the freight forwarder) presents the documents, evidencing full compliance with the terms of the letter of credit to the exporter's bank in Japan.",
+
+
+"13. The bank reviews the documents to make sure that they are in order and have been presented prior to the expiration date. If everything is in order, the bank releases the money to the exporter (Zen Export International).",
+
+"14. The documents are returned to the importer (the applicant 123 Importing via its bank.",
+
+"15. The importer (123 Importing) submits the documents to a Customs broker (an individual who is licensed\n" +
+"to transact Customs business on behalf of others). The Customs broker's responsibilities include the\n" +
+"coordination of clearing the goods through Customs and arranging to have the goods delivered to the\n" +
+"importer.",
+
+"16. In order to have the shipping company release the goods to the Customs broker, the broker must return\n" +
+"the original bill of lading back to the shipping carrier. The shipping company will only release the cargo\n" +
+"when the bill of lading is returned to them. Without the onboard ocean bill of lading, the shipping\n" +
+"company will not release the cargo.",
+
+    "17. After the Customs broker receives the goods, the goods are transported and delivered to the importer\n" +
+    "(123 Importing). This payment term is called a letter of credit drawn at sight, because the bank releases\n" +
+    "the funds to the exporter upon seeing the documents. Bank fees are the responsibility of the importer\n" +
+    "and are usually a percentage of the amount of the letter of credit."
+
+
+]
+
+export default StepTxt;
--- application/src/threadGame.js
+++ application/src/threadGame.js
@@ -1,10 +1,13 @@
 import React, {useState} from 'react';
 import './App.css';
 import BtnContext from "./Components/BtnContext";
+import StepTxt from "./Components/StepTxt";
+
+
 
 
 function Main(props){
-    const[etape, setEtape] = useState(-5);
+    const[etape, setEtape] = useState(0);
     let centre;
 
     if(etape===0) {
@@ -121,7 +124,7 @@ function Main(props){
 
                 {centre}
 
-                <center><footer id = "foot"><Progress /></footer></center>
+                <center><footer id = "foot"></footer></center>
 
 
             </section>
@@ -152,96 +155,16 @@ function Accueil(props){
 
 function EtapeJeu(props){
     let jeu;
-    if(props.etape === -5){
-        jeu = (
-            <>
-           <center> <div id="suite2">
 
-                <p  className="contenuEtape">April 20th: The Importer is called 123 Importing Company, 89 High Street, New York, NY 70036, USA.</p>
-
-        </div>
-               <button id="next" onClick={props.suivante}>Next</button></center></>)
-    }else if(props.etape === -4){
-        jeu = (
-            <>
-            <div id="suite2" >
-
-                <p className="contenuEtape">
-                    <p>It is interested in purchasing 3000 men's cotton shirts from a supplier in Japan, as detailed on Purchase
-                    Orders 7689 (1000 red/blue/white shirts in size S and 1000 red/blue/white shirts in size M) and 7690
-                    (1000 red/blue/white shirts in size L) from a supplier (exporter) called Zen Export International
-                    Company, 31 Hinga, Tokyo, Japan.</p>
-                    <p>The value of this purchase is $30,000 plus 10% VAT.</p>
-                    <p>Zen Export International Company is responsible for all expenses until the point of shipment, Japan. The
-                        commercial invoice number is 3879. </p></p>
-
-            </div>
-                <button id="next" onClick={props.suivante}>Next</button>
-            </>)
-    }else if(props.etape === -3){
+    if(props.etape === 1){
 
         jeu = (
             <>
-            <div id="suite2">
 
-                <p className="contenuEtape"> The shirts will be packed in boxes of 250 shirts.
-
-                    Each box weighing 20 Kg 123 Importing completes the application for a Commercial Letter of Credit from its bank and is called
-                    the applicant; the exporter in Japan, Zen Export International, is called the beneficiary.
-
-                    The last date for presentation of documents for payment to the bank in Japan is May 15th. The last date
-                    for presentation of documents at the bank in New York is May 15th.
-
-                    World Wide Bank, International Operations, P.O. Box 144, Grand Street Station, NY, NY 50008, USA
-                    Cable address: World Wide Bank, account: 123-987654
-
-                    World Wide Bank/Japan, Central P.O. Box 6789, 5-1 Marunouchi 6-Chome, Chiyoda-ku, Tokyo 100,
-                    Japan</p>
-
-            </div>
-                <button id="next" onClick={props.suivante}>Next</button>
-                </>)
-    }else if(props.etape === -2){
-        jeu = (
-            <>
-            <div id="suite2">
-
-                <p  className="contenuEtape"> Documents to be presented to the confirming bank in Japan:
-
-                    - a Commercial Invoice No. 3879 (original and three copies), a full set of original onboard ocean Bill of
-                    Lading no. 9965 marked "Freight Collect!
-
-                    - B/L: packaging instructions (shirts to be sorted per size and model and packed in plastic bags)
-
-                    Freight Forwarder: Asia Freight, P.O. Box 9853, 8 Marunouchi 5-Chame, Chiyoda-ku, Tokyo 100, Japan
-                    Asia Freight is responsible for loading boxes on trailer.
-                </p>
-
-            </div>
-                <button id="next" onClick={props.suivante}>Next</button>
-                </>)
-    }else if(props.etape === -1){
-        jeu = (
-            <>
-            <div id="suite2">
-
-                <center><p className="contenuEtape"> Ocean booking number 839217/10
-
-                    Goods to be collected in port of destination by DML USA (VAT NUMBER: 3939392), Address New York,
-                    New York 2001, USA. Traller number: TYT 2001 JKP.
-                </p></center>
-
-            </div><button id="next" onClick={props.suivante}>Next</button>
-
-            </>)
-    }
-    else if(props.etape === 1){
-
-        jeu = (
-            <>
            <center> <p>Choose the right step.</p>
+
             <div id ="suite"><DevinerSuite className = "acc" suivante={props.suivante}
-                                           possible={[{id: 1, text: "je suis la première carte"},{id: 2, text: "je suis la deuxième carte"},{id: 3, text: "je suis la troisième carte"},{id: 4, text: "je suis la quatrième quatre"}]}
+                                           possible={[{id: 1, text:  StepTxt[0]},{id: 2, text:  StepTxt[3]},{id: 3, text:  StepTxt[15]}]}
                                            reponse={2} /> </div></center> </>)
     } else if(props.etape === 2){
         jeu = (
@@ -302,6 +225,7 @@ function EtapeJeu(props){
             return(
 
             <div className="acc">
+                <Progress />
                 <h1>Actual Step : {props.etape}</h1>
                 {jeu}
             </div>
@@ -332,7 +256,6 @@ function DevinerSuite(props){
                 <div className="cards">
                     <div className="cont">
                         <h2>{txt.id}</h2>
-                        <h3>Card {txt.id}</h3>
                         <p>{txt.text}</p>
 
 
@@ -384,7 +307,7 @@ function Formulaire(props){
         }, 200);
         return (
             <>
-                <div><h2>E-COMM Time</h2></div>
+
            <center> <div className="progress">
                 <div className="progress-done" style={style}>
                 </div>
