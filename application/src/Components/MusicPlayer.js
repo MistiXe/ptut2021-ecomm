522328cec7b1e978dfdf7c7a88f179cc84712121
MistiXe <hohomahdjoub@gmail.com>
2021-11-11 12:04:06 +0100

    Patches E-COMM V_0.3
    Time Bar.
    UI settings updated.

--- application/src/Components/MusicPlayer.js
+++ application/src/Components/MusicPlayer.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import Sound from "react-sound";
 
+
 function Music() {
    return(
        <Sound
--- application/src/Components/ProgressBar.js
+++ application/src/Components/ProgressBar.js
@@ -1,18 +1,27 @@
 import React from "react";
 
-function ProgressBar(props){
+function ProgressBar(props) {
     const {completed} = props;
     const fillerStyles = {
-        width: `${completed*4}%`
+        width: `${completed * 4}%`
     };
-    return (
-        <div className="pBarContainerStyle">
-            <div className="pBarFillerStyle" style={fillerStyles}>
+
+    if (({completed}) < 10) {
+        return (<div className="pBarContainerStyle">
+            <div className="pBarFillerStyle2" style={fillerStyles}>
                 <span className="pBarLabelStyles">{`${completed} days left`}</span>
             </div>
-        </div>
-    );
+        </div>)
+    } else {
+        return (
+            <div className="pBarContainerStyle">
+                <div className="pBarFillerStyle" style={fillerStyles}>
+                    <span className="pBarLabelStyles">{`${completed} days left`}</span>
+                </div>
+            </div>
+        );
 
+    }
 }
 
 export default ProgressBar;
