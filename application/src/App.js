2dd73664889f22bebf75885476a175bfe0d0a115
MistiXe <hohomahdjoub@gmail.com>
2021-11-11 16:21:31 +0100

    Patches E-COMM V_0.3
    Time Bar.
    UI settings updated.

--- application/src/App.js
+++ application/src/App.js
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from 'react';
 import Main from "./threadGame";
 import PreLoader2 from "./Preloader/Preloader2";
 import sound from "./Sound/persephone.mp3";
-import { motion } from 'framer-motion';
+
 
 
 
