efc82df33bd9f9b59a7b7977504ac2662d4fac38
MistiXe <hohomahdjoub@gmail.com>
2021-11-18 18:53:09 +0100

    Patches E-COMM V_0.3
    Time Bar.
    UI settings updated.

--- application/src/App.js
+++ application/src/App.js
@@ -4,7 +4,7 @@ import Main from "./threadGame";
 import PreLoader2 from "./Preloader/Preloader2";
 import Music from "./Components/MusicPlayer";
 import cdlc from "./Sound/chinoisdelacalle.mp3";
-import avion from "./image/piece-en-euro.png";
+
 import CreateParty from "./Server/CreateParty.";
 
 
