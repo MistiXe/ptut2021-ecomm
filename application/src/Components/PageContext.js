e1e05984fadb6e784a76f2074deb22afc41fb33c
MistiXe <hohomahdjoub@gmail.com>
2021-11-23 21:22:11 +0100

    Patches E-COMM V_0.3 New UI for Create a Party Song Cards Updated

--- application/src/Components/PageContext.js
+++ application/src/Components/PageContext.js
@@ -10,42 +10,42 @@ function PageContext(props) {
         return (
             <div>
                 <h1>Context</h1>
-                <div>April 20th: The Importer is called 123 Importing Company, 89 High Street, New York, NY 70036, USA.
+                <div className="textCt"><p>April 20th: The Importer is called 123 Importing Company, 89 High Street, New York, NY 70036, USA.{"\n"}</p>
 
-                    It is interested in purchasing 3000 men's cotton shirts from a supplier in Japan, as detailed on Purchase
-                    Orders 7689 (1000 red/blue/white shirts in size S and 1000 red/blue/white shirts in size M) and 7690
-                    (1000 red/blue/white shirts in size L) from a supplier (exporter) called Zen Export International
-                    Company, 31 Hinga, Tokyo, Japan. The value of this purchase is $30,000 plus 10% VAT. Zen Export
-                    International Company is responsible for all expenses until the point of shipment, Japan. The
-                    commercial invoice number is 3879. The shirts will be packed in boxes of 250 shirts. Each box weighing
-                    20 Kg
+                   <p> It is interested in purchasing 3000 men's cotton shirts from a supplier in Japan, as detailed on Purchase{"\n"}
+                    Orders 7689 <p>(1000 red/blue/white shirts in size S and 1000 red/blue/white shirts in size M) and 7690 {"\n"}
+                    (1000 red/blue/white shirts in size L) from a supplier (exporter) called Zen Export International </p>
+                    <p>Company, 31 Hinga, Tokyo, Japan. The value of this purchase is $30,000 plus 10% VAT. Zen Export {"\n"}
+                    International Company is responsible<p> for all expenses until the point of shipment, Japan. The {"\n"}
+                            commercial invoice number is 3879.</p> The shirts will be packed in boxes of 250 shirts. Each box weighing {"\n"}s
+                       20 Kg</p></p>
 
-                    123 Importing completes the application for a Commercial Letter of Credit from its bank and is called
-                    the applicant; the exporter in Japan, Zen Export International, is called the beneficiary.
+                    <p>123 Importing completes the application for a Commercial Letter of Credit from its bank and is called
+                    the applicant; the exporter in Japan, Zen Export International, is called the beneficiary.</p>
 
-                    The last date for presentation of documents for payment to the bank in Japan is May 15th. The last date
-                    for presentation of documents at the bank in New York is May 15th.
+                <p>The last date for presentation of documents for payment to the bank in Japan is May 15th. The last date
+                    for presentation of documents at the bank in New York is May 15th. </p>
 
-                    World Wide Bank, International Operations, P.O. Box 144, Grand Street Station, NY, NY 50008, USA
-                    Cable address: World Wide Bank, account: 123-987654
+                <p> World Wide Bank, International Operations, P.O. Box 144, Grand Street Station, NY, NY 50008, USA
+                    Cable address: World Wide Bank, account: 123-987654</p>
 
-                    World Wide Bank/Japan, Central P.O. Box 6789, 5-1 Marunouchi 6-Chome, Chiyoda-ku, Tokyo 100,
-                    Japan
+                <p> World Wide Bank/Japan, Central P.O. Box 6789, 5-1 Marunouchi 6-Chome, Chiyoda-ku, Tokyo 100,
+                    Japan. </p>
 
-                    Documents to be presented to the confirming bank in Japan:
+                <p> Documents to be presented to the confirming bank in Japan: </p>
 
-                    - a Commercial Invoice No. 3879 (original and three copies), a full set of original onboard ocean Bill of
-                    Lading no. 9965 marked "Freight Collect!
+                <p> - a Commercial Invoice No. 3879 (original and three copies), a full set of original onboard ocean Bill of
+                    Lading no. 9965 marked "Freight Collect! </p>
 
-                    - B/L: packaging instructions (shirts to be sorted per size and model and packed in plastic bags)
+                <p> - B/L: packaging instructions (shirts to be sorted per size and model and packed in plastic bags)
 
-                    Freight Forwarder: Asia Freight, P.O. Box 9853, 8 Marunouchi 5-Chame, Chiyoda-ku, Tokyo 100, Japan
-                    Asia Freight is responsible for loading boxes on trailer.
+                <p>Freight Forwarder: Asia Freight, P.O. Box 9853, 8 Marunouchi 5-Chame, Chiyoda-ku, Tokyo 100, Japan
+                    Asia Freight is responsible for loading boxes on trailer.</p>
 
-                    Ocean booking number 839217/10
+                    Ocean booking number 839217/10</p>
 
-                    Goods to be collected in port of destination by DML USA (VAT NUMBER: 3939392), Address New York,
-                    New York 2001, USA. Traller number: TYT 2001 JKP
+                   <p> Goods to be collected in port of destination by DML USA (VAT NUMBER: 3939392), Address New York,
+                    New York 2001, USA. Traller number: TYT 2001 JKP</p>
                 </div>
                 <AiFillLeftCircle onClick={() => setPage(2)}/>
                 <AiFillRightCircle onClick={() => setPage((page + 1) % 3)}/>
@@ -62,8 +62,8 @@ function PageContext(props) {
         return (
             <div>
                 <h1>Image Context</h1>
-                <div>
-                    <img src={imageContext} alt="Image Context"/>
+                <div >
+                    <img  src={imageContext} alt="Image Context" className="context"/>
                 </div>
                 <AiFillLeftCircle onClick={() => setPage((page - 1) % 3)}/>
                 <AiFillRightCircle onClick={() => setPage((page + 1) % 3)}/>
