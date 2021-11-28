a51d8bd663628934ca04076bc73145a4b49f9712
MistiXe <hohomahdjoub@gmail.com>
2021-11-28 12:56:21 +0100

    Patches E-COMM V_0.3 New UI for Create a Party Song Cards Updated

--- application/src/Components/PageContext.js
+++ application/src/Components/PageContext.js
@@ -11,20 +11,39 @@ function PageContext(props) {
         return (
             <>
             <div>
-                <div className="cardsF">
+                <div className="contStep">
+                <div className="cardsE">
 
 
-                    <div className="debile"><h3 className="h3Txt">E-</h3><h3>E-</h3>
+                    <div className="debile">
                     </div>
                     <div className="contenuTxtT">
-                        <h3>First Story >></h3>
-                        <h3>First Story >></h3>
+                        <h3>1>></h3>
+                        <h3>1>></h3>
 
                     </div>
 
+                    <div className="info">
+                        <h1>The Beginning...</h1>
 
-                <AiFillLeftCircle onClick={() => setPage(2)}/>
-                <AiFillRightCircle onClick={() => setPage((page + 1) % 3)}/>
+                        <p>
+
+                            <p>April 20th: The Importer is called 123 Importing Company, 89 High Street, New York, NY 70036, USA.{"\n"}</p>
+
+                            <p> It is interested in purchasing 3000 men's cotton shirts from a supplier in Japan, as detailed on Purchase{"\n"}
+                                Orders 7689 <p>(1000 red/blue/white shirts in size S and 1000 red/blue/white shirts in size M) and 7690 {"\n"}
+                                    (1000 red/blue/white shirts in size L) from a supplier (exporter) called Zen Export International </p>
+                                <p>Company, 31 Hinga, Tokyo, Japan. </p></p>
+
+
+                        </p>
+                    </div>
+
+                </div>
+
+                <div className="btnSwitch">
+                <AiFillLeftCircle className="circleBtn"onClick={() => setPage(2)}/>
+                    <AiFillRightCircle className="circleBtn"onClick={() => setPage((page + 1) % 3)}/></div>
                 <br/>
                 <btn onClick={props.retour}>Return</btn>
                 <footer>
@@ -43,8 +62,8 @@ function PageContext(props) {
                 <div >
                     <img  src={imageContext} alt="Image Context" className="context"/>
                 </div>
-                <AiFillLeftCircle onClick={() => setPage((page - 1) % 3)}/>
-                <AiFillRightCircle onClick={() => setPage((page + 1) % 3)}/>
+                <AiFillLeftCircle className="circleBtn"onClick={() => setPage((page - 1) % 3)}/>
+                <AiFillRightCircle className="circleBtn"onClick={() => setPage((page + 1) % 3)}/>
                 <br/>
                 <btn onClick={props.retour}>Return</btn>
                 <footer>
@@ -59,8 +78,8 @@ function PageContext(props) {
             <div>
                 <h1>Tutorial</h1>
                 <h3>How to play ?</h3>
-                <AiFillLeftCircle onClick={() => setPage((page - 1) % 3)}/>
-                <AiFillRightCircle onClick={() => setPage((page + 1) % 3)}/>
+                <AiFillLeftCircle className="circleBtn" onClick={() => setPage((page - 1) % 3)}/>
+                <AiFillRightCircle className="circleBtn" onClick={() => setPage((page + 1) % 3)}/>
                 <br/>
                 <btn onClick={props.retour}>Return</btn>
                 <footer>
