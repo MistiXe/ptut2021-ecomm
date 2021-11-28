ac03cc246ae572411987afb15ce92c78803e16f3
florentgerber <florentgerberpro@gmail.com>
2021-11-28 02:33:32 +0100

    update

--- application/src/Components/Formulaire.js
+++ application/src/Components/Formulaire.js
@@ -3,10 +3,28 @@ import React, {useState} from "react";
 
 
 function Formulaire(props) {
+    const [pageFormu, setPageFormu] = useState(0);//Faut utiliser ça pour changer de morceaux de formulaire
+    if(props.etape===4){
+        //email exercice
+        return (
+            <div onClick={props.suivante}>Next</div>
+        );
 
-return (
-    <div onClick={props.suivante}>Next</div>
-);
+    }
+    else if(props.etape===5){
+        //Application for issuing Letter of Credit (document super long et donc differents des autres)
+        return (
+            <div onClick={props.suivante}>Next</div>
+        );
+
+    }
+    else {
+        //Les autres documents qu'on peut normalement traiter de la même manière
+        return (
+            <div onClick={props.suivante}>Next</div>
+        );
+
+    }
 
 
 }
