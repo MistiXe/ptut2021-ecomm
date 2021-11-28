946ffdf1a894ae6dc19c677f8c80c06af5bffbde
MistiXe <hohomahdjoub@gmail.com>
2021-11-28 12:18:32 +0100

    Patches E-COMM V_0.3 New UI for Create a Party Song Cards Updated

--- application/src/Components/Formulaire.js
+++ application/src/Components/Formulaire.js
@@ -1,11 +1,39 @@
 import React, {useState} from "react";
+import GenerateDoc from "./data/GenerateDoc";
 
 
 
 function Formulaire(props) {
 
+    let formuInst;
+    if(props.etape === 2){
+
+        formuInst = ( <>
+                <body className="bodyForm">
+            <div className="box">
+                <div className="container">
+                    <div className="form">
+                        <h1>Commercial invoice</h1>
+                        <img className="imgformu" src={GenerateDoc.tabCI[0].img}/>
+
+
+                    </div>
+                </div>
+            </div>
+
+                </body>
+
+            </>
+
+        )
+    }
+
 return (
+    <>
+        {formuInst}
     <div onClick={props.suivante}>Next</div>
+    </>
+
 );
 
 
--- application/src/Components/PageContext.js
+++ application/src/Components/PageContext.js
@@ -2,51 +2,27 @@ import React, {useState} from "react";
 import {BsCircleFill, BsCircle} from "react-icons/bs";
 import {AiFillLeftCircle, AiFillRightCircle} from "react-icons/ai"
 import imageContext from "../image/imgContext.jpg";
+import imageicon from "../image/ultimateback.jpg";
 
 
 function PageContext(props) {
     const [page, setPage] = useState(0);
     if (page === 0) {
         return (
+            <>
             <div>
-                <h1>Context</h1>
-                <div className="textCt"><p>April 20th: The Importer is called 123 Importing Company, 89 High Street, New York, NY 70036, USA.{"\n"}</p>
+                <div className="cardsF">
 
-                   <p> It is interested in purchasing 3000 men's cotton shirts from a supplier in Japan, as detailed on Purchase{"\n"}
-                    Orders 7689 <p>(1000 red/blue/white shirts in size S and 1000 red/blue/white shirts in size M) and 7690 {"\n"}
-                    (1000 red/blue/white shirts in size L) from a supplier (exporter) called Zen Export International </p>
-                    <p>Company, 31 Hinga, Tokyo, Japan. The value of this purchase is $30,000 plus 10% VAT. Zen Export {"\n"}
-                    International Company is responsible<p> for all expenses until the point of shipment, Japan. The {"\n"}
-                            commercial invoice number is 3879.</p> The shirts will be packed in boxes of 250 shirts. Each box weighing {"\n"}s
-                       20 Kg</p></p>
 
-                    <p>123 Importing completes the application for a Commercial Letter of Credit from its bank and is called
-                    the applicant; the exporter in Japan, Zen Export International, is called the beneficiary.</p>
+                    <div className="debile"><h3 className="h3Txt">E-</h3><h3>E-</h3>
+                    </div>
+                    <div className="contenuTxtT">
+                        <h3>First Story >></h3>
+                        <h3>First Story >></h3>
 
-                <p>The last date for presentation of documents for payment to the bank in Japan is May 15th. The last date
-                    for presentation of documents at the bank in New York is May 15th. </p>
+                    </div>
 
-                <p> World Wide Bank, International Operations, P.O. Box 144, Grand Street Station, NY, NY 50008, USA
-                    Cable address: World Wide Bank, account: 123-987654</p>
 
-                <p> World Wide Bank/Japan, Central P.O. Box 6789, 5-1 Marunouchi 6-Chome, Chiyoda-ku, Tokyo 100,
-                    Japan. </p>
-
-                <p> Documents to be presented to the confirming bank in Japan: </p>
-
-                <p> - a Commercial Invoice No. 3879 (original and three copies), a full set of original onboard ocean Bill of
-                    Lading no. 9965 marked "Freight Collect! </p>
-
-                <p> - B/L: packaging instructions (shirts to be sorted per size and model and packed in plastic bags)
-
-                <p>Freight Forwarder: Asia Freight, P.O. Box 9853, 8 Marunouchi 5-Chame, Chiyoda-ku, Tokyo 100, Japan
-                    Asia Freight is responsible for loading boxes on trailer.</p>
-
-                    Ocean booking number 839217/10</p>
-
-                   <p> Goods to be collected in port of destination by DML USA (VAT NUMBER: 3939392), Address New York,
-                    New York 2001, USA. Traller number: TYT 2001 JKP</p>
-                </div>
                 <AiFillLeftCircle onClick={() => setPage(2)}/>
                 <AiFillRightCircle onClick={() => setPage((page + 1) % 3)}/>
                 <br/>
@@ -57,6 +33,8 @@ function PageContext(props) {
                     <BsCircle onClick={() => setPage(2)}/>
                 </footer>
             </div>
+            </div>
+                </>
         );
     } else if (page === 1) {
         return (
